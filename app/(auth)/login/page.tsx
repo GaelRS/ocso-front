67b2c47aa78901e15b95fcc0defe6e322d1cc4b5
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-22 16:41:14 -0600

    add constants

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,20 +1,29 @@
+import { API_URL } from "@/constants";
 import {Button, Input} from "@heroui/react";
+import axios from "axios";
 import Link from "next/link";
 
 export default function LoginPage() {
+    const handleSubmit = async (e: React.FormEvent ) =>{
+        e.preventDefault();
+        const formData = new FormData(e.target );
+        const {data} = await axios.post(`${API_URL}/auth/login`, formData)
+        console.log(data);
+        return;
+    }
     return (
-        <div className="bg-orange-500 px-4 py-4 rounded-md">
+        <form className="bg-orange-500 px-4 py-4 rounded-md">
             <p className="text-2xl my-4 text-white">Iniciar sesion</p>
 
             <div className="flex flex-col gap-2 y-4 items-center">
-                <Input label="Email" type="email" isRequired size="sm"/>
-                <Input label="Contraseña" type="password" isRequired size="sm"/>
+                <Input label="Email" name="userEmail" type="email" isRequired size="sm"/>
+                <Input label="Contraseña" name="userPassword" type="password" isRequired size="sm"/>
             </div>
 
             <div className="flex flex-col items-center gap-2">
                 <Button color="primary">Iniciar Sesion</Button>
                 <p className="text-white">¿No tienes una cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link> </p>    
             </div>
-        </div>
+        </form>
     )
 }

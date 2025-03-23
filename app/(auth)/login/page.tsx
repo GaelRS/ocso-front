f1fceedd42a19a284279bf399d46e74a3d2134a4
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-22 22:30:25 -0600

    feat: Cookies un SSC

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,22 +1,36 @@
 "use client";
 import { API_URL } from "@/constants";
-import { Button, Input } from "@heroui/react";
+import { Button, Input, Spinner } from "@heroui/react";
 import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
 
 export default function LoginPage() {
+    const [submitting, setsubmitting] = useState(false);
+    const router = useRouter();
+
     const handleSubmit = async (e: React.FormEvent) => {
+        setsubmitting(true);
         e.preventDefault();
         const formData = new FormData(e.target); 
         let authData: any = {}
         authData.userEmail = formData.get("userEmail");
         authData.userPassword = formData.get("userPassword");
-        const {data} = await axios.post(`${API_URL}/auth/login`,{
-            ...authData
-        },{
-            withCredentials: true
-        });
-        console.log(data);
+        try{
+            const response = await axios.post(`${API_URL}/auth/login`,{
+                ...authData
+            },{
+                withCredentials: true
+            });
+            if(response.status === 201){
+                router.push("/dashboard");
+            }
+            setsubmitting(false);
+        }catch(e){
+            setsubmitting(false);
+        }
         return;
     };
     return (
@@ -29,7 +43,7 @@ export default function LoginPage() {
             </div>
 
             <div className="flex flex-col items-center gap-2">
-                <Button color="primary" type="submit">Iniciar Sesion</Button>
+                <Button color="primary" type="submit" disabled={submitting}> {submitting?"Enviando..." : "Iniciar Sesión"}</Button>
 
                 <p className="text-white">
                     ¿No tienes una cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link>{" "}
--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 const CountPage = async () => {
    const countLocations = await axios.get("http://127.0.0.1:4000/locations") 
    console.log(countLocations.data)
-   return countLocations?.data?.length;
+   const cantidad = countLocations?.data?.length;
+   return `Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}`;
 }
-
+;
 export default CountPage;

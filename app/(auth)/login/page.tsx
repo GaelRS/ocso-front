9edc67b40eeabf4a5502edbeb3229f444967a15d
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-22 21:31:02 -0600

    feat: Send Cookie

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -7,22 +7,18 @@ import Link from "next/link";
 export default function LoginPage() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        const formData = new FormData(e.target as HTMLFormElement); 
-        const userEmail = formData.get("userEmail");
-        const userPassword = formData.get("userPassword");
-
-        try {
-            const { data } = await axios.post(`${API_URL}/auth/login`, {
-              userEmail,
-              userPassword
-            });
-            console.log(data);
-           
-        } catch (error) {
-            console.error("Error al iniciar sesión:", error);
-        }
+        const formData = new FormData(e.target); 
+        let authData: any = {}
+        authData.userEmail = formData.get("userEmail");
+        authData.userPassword = formData.get("userPassword");
+        const {data} = await axios.post(`${API_URL}/auth/login`,{
+            ...authData
+        },{
+            withCredentials: true
+        });
+        console.log(data);
+        return;
     };
-
     return (
         <form className="bg-orange-500 px-4 py-4 rounded-md" onSubmit={handleSubmit}>
             <p className="text-2xl my-4 text-white">Iniciar sesion</p>

55335963f85a79894f4474fa0c395ff3f92b4b66
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-23 01:22:21 -0600

    Cards for Employees Locations

---
+++ app/dashboard/@locations/_components/SelectLocation.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { Select, SelectItem } from "@heroui/react"
+import { Location } from "@/entities"
+import { useRouter } from "next/navigation"
+
+export default function SelectLocation({locations, store}:{locations: Location[], store: string | string[] | undefined}) {
+   
+   const router = useRouter();
+
+   return (
+      <Select placeholder="Selecciona una tienda" label="Tienda" classNames={{
+         mainWrapper: "hover:ring-2 ring-red-300 rounded-xl transition-all",
+         
+      }}
+         
+         selectedKeys={store?store:"0"}
+         onChange={((e)=>{
+            if(e.target.value === "0" || e.target.value === ""){ 
+               router.push(`/dashboard`)
+            }else{
+               router.push(`/dashboard?store=${e.target.value}`)
+            }
+         })}
+      >
+         {locations.map((location:Location) => {
+            return (
+               <SelectItem key={location.locationId} data-value={location.locationId}>
+                  {location.locationName}
+               </SelectItem>
+            )
+         })}
+      </Select>
+   )
+}
---
+++ app/dashboard/@locations/page.tsx
@@ -0,0 +1,36 @@
+
+import axios from "axios";
+import { Location } from "@/entities";
+import SelectLocation from "./_components/SelectLocation";
+
+const LocationsPage = async ({searchParams}:{searchParams: { [key: string]: string | string[] | undefined }}) => {
+   
+   let {data} = await axios.get<Location[]>("http://127.0.0.1:4000/locations",{
+      headers: {
+         "Authorization": "Bearer ${token}"   
+      }
+   });
+
+   data = [
+      {
+         locationId: 0,
+         locationName: "Ninguna",
+         locationLatLng: [0, 0],
+         locationAddress: "Ninguna",
+         employees: []
+      },
+      ...data
+   ]
+   
+   return (
+      <div  className="w-8/12">
+         <div className="w-full flex flex-col items-center h-[90vh]"> 
+            <div className="w-1/2 my-10">
+               <SelectLocation locations={data} store={searchParams?.store} />
+            </div>
+         </div>
+      </div>
+   )
+}
+;
+export default LocationsPage;

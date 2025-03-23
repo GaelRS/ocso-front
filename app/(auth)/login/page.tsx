b867ab399bf293f9c3b61b894af6af6a0effa34e
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-22 19:59:00 -0600

    feat: JWT retrieved

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,29 +1,44 @@
+"use client";
 import { API_URL } from "@/constants";
-import {Button, Input} from "@heroui/react";
+import { Button, Input } from "@heroui/react";
 import axios from "axios";
 import Link from "next/link";
 
 export default function LoginPage() {
-    const handleSubmit = async (e: React.FormEvent ) =>{
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        const formData = new FormData(e.target );
-        const {data} = await axios.post(`${API_URL}/auth/login`, formData)
-        console.log(data);
-        return;
-    }
+        const formData = new FormData(e.target as HTMLFormElement); 
+        const userEmail = formData.get("userEmail");
+        const userPassword = formData.get("userPassword");
+
+        try {
+            const { data } = await axios.post(`${API_URL}/auth/login`, {
+              userEmail,
+              userPassword
+            });
+            console.log(data);
+           
+        } catch (error) {
+            console.error("Error al iniciar sesión:", error);
+        }
+    };
+
     return (
-        <form className="bg-orange-500 px-4 py-4 rounded-md">
+        <form className="bg-orange-500 px-4 py-4 rounded-md" onSubmit={handleSubmit}>
             <p className="text-2xl my-4 text-white">Iniciar sesion</p>
 
             <div className="flex flex-col gap-2 y-4 items-center">
-                <Input label="Email" name="userEmail" type="email" isRequired size="sm"/>
-                <Input label="Contraseña" name="userPassword" type="password" isRequired size="sm"/>
+                <Input label="Email" name="userEmail" type="email" isRequired size="sm" />
+                <Input label="Contraseña" name="userPassword" type="password" isRequired size="sm" />
             </div>
 
             <div className="flex flex-col items-center gap-2">
-                <Button color="primary">Iniciar Sesion</Button>
-                <p className="text-white">¿No tienes una cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link> </p>    
+                <Button color="primary" type="submit">Iniciar Sesion</Button>
+
+                <p className="text-white">
+                    ¿No tienes una cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link>{" "}
+                </p>
             </div>
         </form>
-    )
+    );
 }

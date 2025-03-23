fea9759f3602b0ebca31339fa57fd3664b48ed6c
GaelRS <grosas04@alumnos.uaq.mx>
2025-03-22 22:57:41 -0600

    Cambio de estilos a Sidebar

--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -4,7 +4,7 @@ const CountPage = async () => {
    const countLocations = await axios.get("http://127.0.0.1:4000/locations") 
    console.log(countLocations.data)
    const cantidad = countLocations?.data?.length;
-   return `Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}`;
+   return <div  className="w-2/12">{`Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}`}</div>
 }
 ;
 export default CountPage;

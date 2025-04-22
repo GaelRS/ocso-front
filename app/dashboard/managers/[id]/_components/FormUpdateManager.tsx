600740af8e086d2e9d88a86a9ac50f4db8f5178c
GaelRS <grosas04@alumnos.uaq.mx>
2025-04-22 17:43:49 -0600

    feat: Modal para actualizar manager completado

--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -15,11 +15,12 @@ export default async function FormUpdateManager({manager}:{manager: Manager}){
    })
    const stores = await responseStores.json();
    return (
-      <form action={updateManagerWithId} className="bg-orange-400 rounded-md">
-         <Input defaultValue={manager.managerFullName} placeholder="Marco Aurelio" name="managerFullName"/>
-         <Input defaultValue={manager.managerEmail} placeholder="manager@ocso.com" name="managerEmail"/>
-         <Input defaultValue={String(manager.managerSalary)} placeholder="120000" name="managerSalary"/>
-         <Input defaultValue={manager.managerPhoneNumber} placeholder="4445681299" name="managerPhoneNumber"/>
+      <form action={updateManagerWithId} className="bg-orange-400 rounded-md flex flex-col flex-grow-0 gap-2">
+         <h1>Actualizar Manager</h1>
+         <Input required={true} isRequired label="Nombre completo" defaultValue={manager.managerFullName} placeholder="Marco Aurelio" name="managerFullName"/>
+         <Input required={true} isRequired label="Correo electronico" defaultValue={manager.managerEmail} placeholder="manager@ocso.com" name="managerEmail"/>
+         <Input required={true} isRequired label="Salario" defaultValue={String(manager.managerSalary)} placeholder="120000" name="managerSalary"/>
+         <Input required={true} isRequired label="Número de telefono" defaultValue={manager.managerPhoneNumber} placeholder="4445681299" name="managerPhoneNumber"/>
          <SelectStore stores={stores} defaultStore={manager?.location?.locationId}/>
          <Button color="primary" type="submit">Actualizar</Button>
          
--- app/dashboard/managers/[id]/_components/SelectStore.tsx
+++ app/dashboard/managers/[id]/_components/SelectStore.tsx
@@ -5,12 +5,12 @@ import { Select, SelectItem } from "@heroui/react";
 
 export default function SelectStore({stores, defaultStore}:{stores:Location[], defaultStore: number}){
    const disableStores = stores.map((store: Location)=>{
-      if(store.managerId !== undefined){
+      if(store.managerId !== undefined && store.locationId !== defaultStore){
          return String(store.locationId);
       }
-   }).filter((storeId)=> storeId !== undefined) 
+   }).filter((storeId)=> storeId !== undefined ) 
    return (
-      <Select defaultSelectedKeys={defaultStore ? [defaultStore] : undefined} disabledKeys={disableStores} >
+      <Select label="Tienda" name="location" defaultSelectedKeys={defaultStore ? [defaultStore] : undefined} disabledKeys={disableStores} >
          {
             stores.map((store: Location) => (
                <SelectItem key={store.locationId}> {store.locationName}</SelectItem>

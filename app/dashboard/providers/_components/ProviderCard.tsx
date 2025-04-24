5d1486d1b73d1d7a22b5eb49779d40a924dc8d35
GaelRS <grosas04@alumnos.uaq.mx>
2025-04-24 10:25:03 -0600

    feat: Página de proveedores

---
+++ app/dashboard/providers/_components/ProviderCard.tsx
@@ -0,0 +1,22 @@
+import { Provider } from "@/entities";
+import { Card, CardHeader, Divider, CardBody } from "@heroui/react";
+
+export default function ProviderCard({provider}:{provider: Provider}){
+   return(
+      <Card >
+         <CardHeader>{provider.providerName}</CardHeader>
+         <Divider/>
+         <CardBody>
+            <p>Correo electrónico: </p>
+            <b>{provider.providerEmail}</b>
+            <p>Número de telefono: </p>
+            <b>{provider.providerPhoneNumber}</b>
+            {
+               provider.products ? (
+                  <p>Tiene <b>{provider.products.length}</b> productos</p>
+               ) : <p>No tiene productos</p>
+            }
+         </CardBody>
+      </Card>
+   )
+}
